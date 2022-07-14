b69df4103582d15504f089c845f7c6ca84076ab5
solovyovrv <solovjevrv@gmail.com>
2022-07-14 23:35:05 +0300

    Решена задача № 7

---
+++ lesson01/task_007/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+Задача № 7
+Выяснить является ли число чётным.
+*/
+
+System.Console.WriteLine("Введите число:");
+string? temp = Console.ReadLine();
+int a = Convert.ToInt32(temp);
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Вы ввели четное число!");
+}
+else
+{
+    Console.WriteLine("Введенное число не является четным!");
+}
