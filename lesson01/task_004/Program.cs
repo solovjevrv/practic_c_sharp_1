690a89a3c842206dcff6a91283082ecf17917978
solovyovrv <solovjevrv@gmail.com>
2022-07-14 23:10:43 +0300

    Решена задача № 4 (неоптимальный способ)

---
+++ lesson01/task_004/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача № 4
+По заданному с клавиатуры номеру дня недели вывести его название
+*/
+
+System.Console.WriteLine("Введите число:");
+string? temp = Console.ReadLine();
+int num = Convert.ToInt32(temp);
+
+if (num  == 1)
+{
+    System.Console.WriteLine("Понедельник");
+}
+if (num  == 2)
+{
+    System.Console.WriteLine("Вторник");
+}
+if (num  == 3)
+{
+    System.Console.WriteLine("Среда");
+}
+if (num  == 4)
+{
+    System.Console.WriteLine("Четверг");
+}
+if (num  == 5)
+{
+    System.Console.WriteLine("Пятница");
+}
+if (num  == 6)
+{
+    System.Console.WriteLine("Суббота");
+}
+if (num  == 7)
+{
+    System.Console.WriteLine("Воскресенье");
+}
+
