f378ff4908df76a704d96f42947b34f67add1298
solovyovrv <solovjevrv@gmail.com>
2022-07-14 22:14:43 +0300

    Решена задача №2

---
+++ lesson01/task_002/Program.cs
@@ -0,0 +1,27 @@
+﻿/* 
+Задача № 2
+С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго
+*/
+System.Console.WriteLine("Введите первое число:");
+string? temp = Console.ReadLine();
+System.Console.WriteLine("Введите второе число:");
+
+int number1 = Convert.ToInt32(temp);
+temp = Console.ReadLine();
+int number2 = Convert.ToInt32(temp);
+
+if (number2 == number1 * number1)
+{
+    System.Console.WriteLine($"Число {number2} является квадратом числа {number1}");
+}
+else
+{
+    if (number1 == number2 * number2)
+    {
+        System.Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+    }
+    else
+    {
+			System.Console.WriteLine("Ни одно число не является квадратом другого");
+    }
+}
