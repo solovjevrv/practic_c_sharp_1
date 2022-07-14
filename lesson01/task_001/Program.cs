c6d11291b0618bea271dc43f66e764d88a6730b4
solovyovrv <solovjevrv@gmail.com>
2022-07-14 21:53:33 +0300

    Решена задача № 1

---
+++ lesson01/task_001/Program.cs
@@ -0,0 +1,14 @@
+﻿/* 
+Задача № 1
+С клавиатуры вводится целое число. Вывести квадрат числа 
+*/
+
+double a;
+double b;
+System.Console.WriteLine("Введите число:");
+string? s = Console.ReadLine();
+a = Convert.ToDouble(s);
+
+b = a * a;
+
+System.Console.WriteLine($"Квадрат числа {a} равен: {b}");
