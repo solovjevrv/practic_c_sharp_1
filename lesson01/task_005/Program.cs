5ac74d9394a5aad7d12d92565c1cf3cf717ff92f
solovyovrv <solovjevrv@gmail.com>
2022-07-14 23:28:11 +0300

    Решена задача № 5

---
+++ lesson01/task_005/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача № 5
+С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+*/
+
+System.Console.WriteLine("Введите первое число:");
+string? temp = Console.ReadLine();
+System.Console.WriteLine("Введите второе число:");
+int num1 = Convert.ToInt32(temp);
+temp = Console.ReadLine();
+int num2 = Convert.ToInt32(temp);
+System.Console.WriteLine("Введите третье число:");
+temp = Console.ReadLine();
+int num3 = Convert.ToInt32(temp);
+
+int max = num1;
+
+if (num1 > num2 && num1 > num3)
+{
+    max = num1;
+    System.Console.WriteLine($"Масимальное первое число - {max}");
+}
+else if (num2 > num3 && num2 > num1)
+{
+    max = num2;
+    System.Console.WriteLine($"Масимальное второе число - {max}");
+}
+else
+{
+    max = num3;
+    System.Console.WriteLine($"Масимальное третье число - {max}");
+}
