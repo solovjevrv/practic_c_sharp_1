3f42a6450fa24c54dc97c99bf8d99e6d62bd3911
solovyovrv <solovjevrv@gmail.com>
2022-07-15 21:13:16 +0300

    Решена задача № 9

---
+++ lesson01/task_009/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача № 9
+Вывести на экран четные числа от 1 до N
+*/
+int start = 1;
+int end = 30;
+while (start <= end)
+{
+    if (start % 2 == 0)
+    {
+        System.Console.Write($"{start} ");
+    }
+
+    start++;
+}
