654e4021d87525f19d4e4939c544f5678a501af8
solovyovrv <solovjevrv@gmail.com>
2022-07-15 21:32:01 +0300

    Решена задача № 10

---
+++ lesson01/task_010/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Задача № 10
+Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+*/
+System.Console.WriteLine("Введите число:");
+string? temp = Console.ReadLine();
+int num = Convert.ToInt32(temp);
+
+int last_digit = num % 10;
+
+System.Console.WriteLine($"Последняя цифра введенного числа: {last_digit}");
