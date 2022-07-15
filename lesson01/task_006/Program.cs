7ce61d635d52bd49c232f52a7367236a5df6b7da
solovyovrv <solovjevrv@gmail.com>
2022-07-15 21:28:23 +0300

    Решена задача № 6

---
+++ lesson01/task_006/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+Задача № 6
+Написать программу вычисления значения функции y = sin(a). (Класс Math). 
+*/
+System.Console.WriteLine("Введите число:");
+string? temp = Console.ReadLine();
+double a = Convert.ToDouble(temp);
+double y = Math.Sin(a);
+System.Console.WriteLine(($"Синус введенного числа равен: {y}"));
