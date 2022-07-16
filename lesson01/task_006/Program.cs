9a77caf55c5e91222f37e16d81d8d392adcb8205
solovyovrv <solovjevrv@gmail.com>
2022-07-16 14:23:58 +0300

    Дополнено  решение задачи №6

--- lesson01/task_006/Program.cs
+++ lesson01/task_006/Program.cs
@@ -7,3 +7,17 @@ string? temp = Console.ReadLine();
 double a = Convert.ToDouble(temp);
 double y = Math.Sin(a);
 System.Console.WriteLine(($"Синус введенного числа равен: {y}"));
+
+
+// решение через функцию
+// System.Console.WriteLine("Введите число:");
+// string? temp = Console.ReadLine();
+// double a = Convert.ToDouble(temp);
+// double Sin(double a)
+// {
+//     return Math.Sin(a);
+// }
+// double y=Sin(a);
+// System.Console.WriteLine(y);
+
+
