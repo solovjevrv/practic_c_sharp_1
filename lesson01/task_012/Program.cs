118ec0bf91bd7b64e84b7bb889f778ef8dc9d9c5
solovyovrv <solovjevrv@gmail.com>
2022-07-16 19:58:32 +0300

    Решена задача  № 12

--- lesson01/task_012/Program.cs
+++ lesson01/task_012/Program.cs
@@ -4,10 +4,11 @@
 */
 
 System.Console.WriteLine("Введите число от 10 до 99: ");
-string? temp = Console.ReadLine();
-int num = Convert.ToDouble(temp);
 
-// int first_digit = num / 10;
-// int last_digit = num % 10;
+int num = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine(num / 10, num % 10);
+int first_digit = num / 10;
+int last_digit = num % 10;
+
+System.Console.WriteLine(first_digit);
+System.Console.WriteLine(last_digit);
