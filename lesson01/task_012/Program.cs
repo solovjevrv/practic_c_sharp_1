e413d8ce4eb12a8e4d40c969103fddf6af2fb567
solovyovrv <solovjevrv@gmail.com>
2022-07-16 14:30:13 +0300

    не решена 12 задача

---
+++ lesson01/task_012/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Задаяча № 12
+С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+*/
+
+System.Console.WriteLine("Введите число от 10 до 99: ");
+string? temp = Console.ReadLine();
+int num = Convert.ToDouble(temp);
+
+// int first_digit = num / 10;
+// int last_digit = num % 10;
+
+System.Console.WriteLine(num / 10, num % 10);
