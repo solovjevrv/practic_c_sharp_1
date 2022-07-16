6f87a43b814d88311df0482eba9ea3e658f1ecd2
solovyovrv <solovjevrv@gmail.com>
2022-07-16 16:52:08 +0300

    Решена задача № 28

---
+++ lesson02/task_028/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача № 28
+Определить количество цифр в числе. Сделать подпрограмму.
+*/
+System.Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int CountDigits(int N)
+{
+    if (N == 0) return 1;
+    int Count = 0;
+    while (N != 0)
+    {
+        Count++;
+        N = N / 10;
+    }
+    return Count;
+}
+System.Console.WriteLine(CountDigits(N));
