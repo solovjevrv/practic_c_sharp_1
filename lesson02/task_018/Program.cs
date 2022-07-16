b97474c216682eccddcb643bd1996c60e2631333
solovyovrv <solovjevrv@gmail.com>
2022-07-16 15:52:57 +0300

    Решена задача № 18

---
+++ lesson02/task_018/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача № 18
+По двум заданным числам проверять является ли одно квадратом другого. Используйте подпрограмму.
+*/
+bool IsSquare(int x, int y)
+{
+    return x * x == y;
+}
+
+System.Console.WriteLine("Введите первое число: ");
+string? temp = Console.ReadLine();
+int x = Convert.ToInt32(temp);
+System.Console.WriteLine("Введите второе число: ");
+temp = Console.ReadLine();
+int y = Convert.ToInt32(temp);
+
+if (IsSquare(x, y))
+{
+    System.Console.WriteLine($"Число {x} является квадратом {y} ");
+}
+else
+{
+    if (IsSquare(y, x))
+    {
+        System.Console.WriteLine($"Число {y} является квадратом {x} ");
+    }
+    else
+    {
+        System.Console.WriteLine("Ни одно число не является квадратом другого");
+    }
+}
