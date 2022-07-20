be31037610bc66be18dc729cf2336e2a1a902bad
solovjevrv <solovjevrv@gmail.com>
2022-07-20 23:32:07 +0300

    Решена задача №26

---
+++ lesson02/task_026/Program.cs
@@ -0,0 +1,19 @@
+﻿/**
+Задача № 26
+Найти сумму чисел от 1 до А
+*/
+
+System.Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+int getSum (int num)
+{
+    for (int i = 1; i<=num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+
+}
+System.Console.WriteLine($"Сумма чисел от 1 до {num} равна: {getSum(num)}");
