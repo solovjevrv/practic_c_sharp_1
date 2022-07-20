db50462bf6ec01eea17865cb0ab084d97632ef65
solovjevrv <solovjevrv@gmail.com>
2022-07-20 23:10:26 +0300

    Решена задача №24

---
+++ lesson02/task_024/Program.cs
@@ -0,0 +1,14 @@
+﻿/**
+Задача № 24
+Вывести на экран таблицу квадратов чисел от 1 до N
+*/
+
+System.Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i<=num; i++)
+{
+    System.Console.WriteLine($"{i}^2 = {i*i}");
+}
+
+
