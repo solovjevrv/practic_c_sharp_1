96dc298967db005ed541e086859039dd00472be4
solovjevrv <solovjevrv@gmail.com>
2022-07-20 23:56:02 +0300

    Решена задача №27

---
+++ lesson02/task_027/Program.cs
@@ -0,0 +1,22 @@
+﻿/**
+Задача № 27
+Возведите число А в натуральную степень B используя цикл
+*/
+
+System.Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+int exp = 1;
+int getExp (int A, int B)
+{
+    for (int i = 1; i<=B; i++)
+    {
+        exp *= A * A;
+    }
+    return exp;
+
+}
+System.Console.WriteLine(getExp(A, B));
+
+//$"Сумма чисел от 1 до {num} равна: {getSum(num)}"
