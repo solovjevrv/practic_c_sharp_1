bc3d66c54a76274d7adf959b07e84ebc5cd0cc4b
solovjevrv <solovjevrv@gmail.com>
2022-07-21 00:09:08 +0300

    Решена задача №29

---
+++ lesson02/task_029/Program.cs
@@ -0,0 +1,27 @@
+﻿/**
+Задача № 29
+Подсчитать сумму цифр в числе
+*/
+
+System.Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+int sumDigit (int num)
+{
+    if (num != 0) 
+    {
+        while (num > 0)
+        {
+						int last_digit = num % 10;
+            num = num / 10;
+            sum += last_digit;
+
+        }
+        return sum;
+    }
+    else return num;
+
+}
+System.Console.WriteLine(sumDigit(num));
+
