ca4621684ad9ae92bc2c7d62f5faa6b32992ce81
solovjevrv <solovjevrv@gmail.com>
2022-07-20 23:23:55 +0300

    Решена задача №25

---
+++ lesson02/task_025/Program.cs
@@ -0,0 +1,12 @@
+﻿/**
+Задача № 25
+Вывести на экран кубы чисел от 1 до N
+*/
+
+System.Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i<=num; i++)
+{
+    System.Console.WriteLine($"{i}^3 = {Math.Pow(i, 3)}");
+}
