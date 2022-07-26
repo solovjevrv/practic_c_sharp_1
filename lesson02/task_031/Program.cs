1dced9330e98cecd9f3df9b578d8a6b7a4329b72
solovjevrv <solovjevrv@gmail.com>
2022-07-26 09:39:40 +0300

    Решена задача № 31

---
+++ lesson02/task_031/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача № 31
+Показать кубы чисел, заканчивающихся на четную цифру
+т. к. не задан диапазон чисел, выберем произвольный [1: N]
+*/
+
+System.Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int cubeN;
+for (int i = 1; i <= N; i++)
+{
+	cubeN = i*i*i;
+    if (cubeN % 10 % 2 == 0)
+    {
+        System.Console.WriteLine(cubeN);
+    }
+   
+}
