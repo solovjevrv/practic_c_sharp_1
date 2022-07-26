af1ab3acd7b6f63d02b35dd8f5281309b580863a
solovjevrv <solovjevrv@gmail.com>
2022-07-26 09:17:23 +0300

    Решена задача № 30

---
+++ lesson02/task_030/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача № 30
+Написать программу вычисления произведения чисел от 1 до N
+*/
+
+
+int multi = 1;
+
+System.Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+	multi *= i;
+}
+System.Console.WriteLine($"Произведение чисел от 1 до {N} равно {multi}!");
