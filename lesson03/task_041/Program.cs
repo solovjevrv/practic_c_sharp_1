1467df2e635da12b1d8aa8833e523d94e2069b72
solovjevrv <solovjevrv@gmail.com>
2022-07-28 21:00:54 +0300

    Решена задача № 41

---
+++ lesson03/task_041/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Задача № 36
+В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+*/
+
+int [] a = new int[8] {-1, 2, 3, 4, -5, 6, 7, 8};
+int maxNum = a.Max();
+int minNum = a.Min();
+for (int i = 0; i < a.Length; i++) {
+    System.Console.Write($"{a[i]} ");
+}
+System.Console.WriteLine($"\n{maxNum - minNum}");
