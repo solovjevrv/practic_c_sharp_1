8ba876ba670ee67980881ccc9e7c80655829cd33
solovjevrv <solovjevrv@gmail.com>
2022-07-31 12:27:39 +0300

    Создана папка по 4 уроку. Решены задачи № 42 и № 44

---
+++ lesson04/task_044/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача № 44
+Написать программу преобразования десятичного числа в двоичное
+*/
+System.Console.WriteLine("Enter number...");
+int num = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine(DecToBin(num));;
+string DecToBin(int num)
+{
+    if (num == 0) return "0";
+    string s = "";
+    while (num != 0)
+    {
+        s = (num % 2) + s;
+        num /= 2;
+    }
+    return s;
+}
