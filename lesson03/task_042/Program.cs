b151a80b97af5c95846864bd4edd233cd63e9a32
solovjevrv <solovjevrv@gmail.com>
2022-07-31 13:49:58 +0300

    временный комит

---
+++ lesson03/task_042/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача № 42
+Выяснить являются ли три числа сторонами треугольника
+*/
+if (isTriangle(a, b, c) == true)
+{
+    System.Console.WriteLine("Числа являются сторонами треугольника!");
+}
+else System.Console.WriteLine("Числа не являются сторонами треугольника!");
+
+
+bool isTriangle (double a, double b, double c)
+{
+    return (a + b > c && b + c > a && a + c > b);
+}
+
+System.Console.WriteLine("Введите первое число...");
+double a = double.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число...");
+double b = double.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите третье число...");
+double c = double.Parse(Console.ReadLine());
+
---
+++ lesson03/task_044/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача № 44
+Написать программу преобразования десятичного числа в двоичное
+*/
+System.Console.WriteLine("Enter number...");
+int num = int.Parse(Console.ReadLine());
+int[] a = new int[10];
+while (num <= 0) 
+{
+    num = num % 2;
+    for (int i = 0; i < a.Length; i++)
+    {
+        a[i] = num;
+        System.Console.Write($"{a[i]} ");
+    }
+    
+}
+
+
