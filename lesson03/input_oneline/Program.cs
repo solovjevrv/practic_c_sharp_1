927ae2c2c91fbe2391f8e58371cb83e90cf94128
solovjevrv <solovjevrv@gmail.com>
2022-07-31 13:04:01 +0300

    создан пример ввода в одну строку

---
+++ lesson03/input_oneline/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+
+Шаблон ввода данных с одной строки
+*/
+
+string s = Console.ReadLine();
+string[] ss = s.Split(' ');
+int[] a = new int[ss.Length];
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = int.Parse(ss[i]);    
+}
+for (int i = 0; i < a.Length; i++)
+{
+    System.Console.WriteLine(a[i]);
+}
