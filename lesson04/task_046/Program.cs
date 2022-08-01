ffba98a85764c7caa490a3d9ec8ede7e96652cf4
solovjevrv <solovjevrv@gmail.com>
2022-08-01 22:14:32 +0300

    1

---
+++ lesson04/task_046/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача № 46
+Показать числа Фибоначчи
+*/
+System.Console.WriteLine("Сколько чисел будем выводить?");
+int N = int.Parse(Console.ReadLine());
+int fib1 = 1;
+int fib2 = 1;
+int fibSum = 0;
+int [] arr = new int[N];
+int i = 0;
+while (N-2 < 0)
+{
+    fibSum = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = fibSum;
+    i = i + 1;
+    System.Console.WriteLine(fib2);
+}
+
