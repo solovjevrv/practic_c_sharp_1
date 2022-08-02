da0f1a32f73a6855a9dce0e181655fc90558ff7e
solovjevrv <solovjevrv@gmail.com>
2022-08-02 21:31:10 +0300

    Решена задача № 48

---
+++ lesson04/task_048/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Задача № 48
+Написать программу копирования массива
+*/
+int[] Arr = { 5, 4, 7, 2, 9 };
+
+int[] Copy = new int[Arr.Length];
+for (int i = 0; i < Arr.Length; i++) {
+    Copy[i] = Arr[i];
+    Console.Write($"{Copy[i]} ");
+}
+
