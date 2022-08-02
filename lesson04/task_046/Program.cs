cd5b36b1c2e7dd4febd5fffee060fc7b8ec6464c
solovjevrv <solovjevrv@gmail.com>
2022-08-02 21:58:05 +0300

    Решена задача № 46

--- lesson04/task_046/Program.cs
+++ lesson04/task_046/Program.cs
@@ -2,19 +2,12 @@
 Задача № 46
 Показать числа Фибоначчи
 */
-System.Console.WriteLine("Сколько чисел будем выводить?");
-int N = int.Parse(Console.ReadLine());
-int fib1 = 1;
-int fib2 = 1;
-int fibSum = 0;
-int [] arr = new int[N];
-int i = 0;
-while (N-2 < 0)
-{
-    fibSum = fib1 + fib2;
-    fib1 = fib2;
-    fib2 = fibSum;
-    i = i + 1;
-    System.Console.WriteLine(fib2);
+Console.Write("Введите конец диапазона от 1 до : ");
+int end=int.Parse(Console.ReadLine());
+int j=1;
+
+for(int i=1; i<=end; i+=j) {
+	Console.Write("{0} ", i);
+	j=i-j;
 }
 
